a30567e8706c038080be8d79e4f9e12b6093c357
Mary Kohn <marymkohn@gmail.com>
2023-04-26 12:58:18 -0400

    add comments

--- calculator.js
+++ calculator.js
@@ -1,27 +1,27 @@
-$(document).ready(function() {
-    let display = $('#display');
-    let currentInput = '';
+$(document).ready(function() { // event listener, waiting for DOM to load
+    let display = $('#display'); // <input> has id="display", note: display is READ ONLY
+    let currentInput = ''; // will be used to store user input
 
-    $('#buttons button').on('click', function() { //event listener for button click
-        let buttonValue = $(this).text(); //button pushed is saved
+    $('#buttons button').on('click', function() { //event listener to buttons, listens for a click
+        let buttonValue = $(this).text(); // retrieves the value of the button clicked
 
-        if (buttonValue === 'C') { // checks if user pushed clear
-            currentInput = ''; // resets user input
+        if (buttonValue === 'C') { // if user pushed clear
+            currentInput = ''; // resets currentInput
         } else {
-            currentInput += buttonValue; // adds values
+            currentInput += buttonValue; // if not, the value to the currentInput
         }
 
-        display.val(currentInput); // displays value of new current input
+        display.val(currentInput); // sets the display to the new current input
     });
 
     $('#equals').on('click', function() { // event listener for equal button
         try {
-            let result = eval(currentInput);
-            display.val(result);
-            currentInput = '';
-        } catch (e) {
-            display.val('Error'); // displays error if NaN
-            currentInput = '';
+            let result = eval(currentInput); // evaluates the currentInput and stores it in 'result'
+            display.val(result); // sets the display to the result
+            currentInput = ''; // resets currentInput
+        } catch (e) { // if an exception is caught
+            display.val('Error'); // show 'Error' on the display
+            currentInput = ''; // reset currentInput
         }
     });
 });
